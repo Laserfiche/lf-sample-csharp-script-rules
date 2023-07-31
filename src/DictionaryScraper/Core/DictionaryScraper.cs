a23d1956c3ea3fb2e461045c7a86b69a162d8cec
Kern.Ding <kern.ding@laserfiche.com>
2023-07-31 15:41:02 -0700

    Copy dictionary scraper

---
+++ src/DictionaryScraper/Core/DictionaryScraper.cs
@@ -0,0 +1,95 @@
+﻿namespace DictionaryScraper.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Threading.Tasks;
+    using OpenScraping;
+    using OpenScraping.Config;
+
+    public class DictionaryScraper
+    {
+        private const string DictionaryDotComDefinitionConfig = @"
+        {
+            'definitions':
+            {
+                '_xpath': '//html//body//div[1]//div//div//div[2]//div//main//section//section//div[1]//section[2]//div//div',
+                'definition': './/span[last()]'
+            }
+        }";
+
+        private const string WordInputKey = "Word";
+
+        public Task<IDictionary<string, object>> GetDefinition(IDictionary<string, object> args)
+        {
+            if (args.TryGetValue(WordInputKey, out var word))
+            {
+                this.TryScrapeWebDictionary(word.ToString(), out List<string> definitions, out string error);
+                return Task.FromResult<IDictionary<string, object>>(this.GenerateResponse(definitions, error, args));
+            }
+
+            return Task.FromResult<IDictionary<string, object>>(this.GenerateResponse(new List<string>(), "Required parameter 'word' not found.", args));
+
+        }
+
+        private IDictionary<string, object> GenerateResponse(List<string> definitions, string error, IDictionary<string, object> args)
+        {
+            return new Dictionary<string, object>
+                {
+                    ["Error"] = error,
+                    ["Definitions"] = definitions,
+                    ["ReceivedArgs"] = args
+                 };
+        }
+
+        private void TryScrapeWebDictionary(string word, out List<string> definitions, out string error)
+        {
+            try
+            {
+                definitions = this.ScrapeDefinitions(word);
+                error = null;
+            }
+            catch (Exception ex)
+            {
+                definitions = new List<string>();
+                error = ex.Message;
+            }
+        }
+
+        private List<string> ScrapeDefinitions(string word)
+        {
+            List<string> definitions = new List<string>();
+            var config = StructuredDataConfig.ParseJsonString(DictionaryDotComDefinitionConfig);
+            var html = this.GetHTML($"https://www.dictionary.com/browse/{word}?s=t").GetAwaiter().GetResult();
+            var extractor = new StructuredDataExtractor(config);
+            var scrapingResults = extractor.Extract(html);
+            foreach (var result in scrapingResults.SelectToken("definitions").Children())
+            {
+                try
+                {
+                    var definition = result.SelectToken("definition");
+                    definitions.Add(definition.ToString());
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"Error parsing definition: {result}");
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
+            return definitions;
+        }
+
+        private async Task<string> GetHTML(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ test/DictionaryScraper.Tests/DictionaryScraperTests.cs
@@ -0,0 +1,51 @@
+﻿namespace DictionaryScraper.Tests;
+
+using DictionaryScraper.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[TestClass]
+public class DictionaryScraperTests
+{
+    [TestMethod]
+    public async Task ResultsShouldHaveSpecificKeysGivenGetDefinitionCall()
+    {
+        var sut = new DictionaryScraper();
+        var result = await sut.GetDefinition(new Dictionary<string, object> { ["Word"]="quixotic"});
+        Assert.IsTrue(result.ContainsKey("Definitions"));
+        Assert.IsTrue(result.ContainsKey("Error"));
+        Assert.IsTrue(result.ContainsKey("ReceivedArgs"));
+    }
+
+    [TestMethod]
+    public async Task ResultsShouldEchoReceivedArguments()
+    {
+        var sut = new DictionaryScraper();
+        var args = new Dictionary<string, object> { ["Word"] = "suplerflous" };
+        var result = await sut.GetDefinition(args);
+        result.TryGetValue("ReceivedArgs", out var receivedArgs);
+        Assert.AreEqual(args, receivedArgs);
+    }
+
+    [TestMethod]
+    public async Task SuccessfulParseShouldNotContainErrors()
+    {
+        var sut = new DictionaryScraper();
+        var args = new Dictionary<string, object> { ["Word"] = "tangible" };
+        var result = await sut.GetDefinition(args);
+        result.TryGetValue("Error", out var receivedError);
+        Assert.AreEqual(null, receivedError);
+    }
+
+    [TestMethod]
+    public async Task SuccessfulParseShouldContainDefinitions()
+    {
+        var sut = new DictionaryScraper();
+        var args = new Dictionary<string, object> { ["Word"] = "time" };
+        var result = await sut.GetDefinition(args);
+        result.TryGetValue("Definitions", out var receivedDefs);
+        var definitionList = (List<string>)receivedDefs;
+        Assert.IsNotNull(receivedDefs);
+        Assert.IsTrue(definitionList.Count > 0);
+    }
+}
